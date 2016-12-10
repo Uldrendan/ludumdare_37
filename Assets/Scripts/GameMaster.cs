130e5026623290e620e93f8580527497f38471a8
Daniel Clarke <danclarke88@gmail.com>
2016-12-09 23:00:53 -0500

    Committing game master and some scripts for UI

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster instance;
+
+	public int energy = 100;
+	public int hygiene = 100;
+	public int food = 100;
+
+	void Start () {
+		if (instance != null)
+			GameObject.Destroy(instance);
+		else
+			instance = this;
+	}
+}
---
+++ Assets/Scripts/ResourceBar.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ResourceBar : MonoBehaviour
+{
+
+	RectTransform rekt;
+
+	float originalSizeX;
+
+	public GameObject resourceFrame;
+
+	bool lowResource = false;
+
+	public Color pulseColor;
+	Color targetColor;
+
+	void Start()
+	{
+		targetColor = pulseColor;
+		rekt = this.GetComponent<RectTransform>();
+		originalSizeX = rekt.sizeDelta.x;
+	}
+
+	void FixedUpdate()
+	{
+		rekt.sizeDelta = new Vector2(originalSizeX * (GameMaster.instance.energy / 100), rekt.sizeDelta.y); //need to make this flexible for other resource types
+
+		if (GameMaster.instance.energy <= 20)
+			lowResource = true;
+		else
+			lowResource = false;
+
+		if (lowResource)
+		{
+			Pulse();
+		}
+		else if (this.GetComponent<Image>().color != Color.white)
+		{
+			ResetColor();
+		}
+	}
+
+	void Pulse()
+	{
+		this.GetComponent<Image>().color = Color.Lerp(this.GetComponent<Image>().color, targetColor, 20 * Time.deltaTime);
+		resourceFrame.GetComponent<Image>().color = Color.Lerp(this.GetComponent<Image>().color, targetColor, 20 * Time.deltaTime);
+
+		if (this.GetComponent<Image>().color == targetColor)
+		{
+			if (targetColor == pulseColor)
+				targetColor = Color.white;
+			else
+				targetColor = pulseColor;
+		}
+	}
+
+	public void ResetColor()
+	{
+		this.GetComponent<Image>().color = Color.white;
+		resourceFrame.GetComponent<Image>().color = Color.white;
+	}
+}
