cfee2e68da3fdb8b2ae611c651da05f183f86d2c
Liam Collins <liamcollins333@gmail.com>
2016-12-09 23:25:45 -0500

    combined PlayerController into GameMaster

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -16,4 +16,28 @@ public class GameMaster : MonoBehaviour {
 		else
 			instance = this;
 	}
+
+	// Update is called once per frame
+	void Update () {
+		CheckClick ();
+	}
+
+	void CheckClick () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 1000f)) {
+				Debug.Log ("Clicked " + hit.collider.gameObject.name);
+				Interactable interactable = hit.collider.gameObject.GetComponent<Interactable> ();
+				if (interactable != null) {
+					Debug.Log ("Interactable");
+					interactable.OnClick ();
+				} else {
+					Debug.Log ("NOT Interactable");
+				}
+			} else {
+				Debug.Log ("Clicked EMPTY SPACE");
+			}
+		}
+	}
 }
