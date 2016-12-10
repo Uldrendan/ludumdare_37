bd6853be0b57f5e144e0905feb6e20485c29a680
Daniel Clarke <danclarke88@gmail.com>
2016-12-10 17:24:48 -0500

    Posting script to have a text object display current time and optionally date

---
+++ Assets/Scripts/ClockView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockView : MonoBehaviour {
+
+	Text clockDisplay;
+	string dateTime;
+	public bool displayDate;
+
+	void Start () {
+		clockDisplay = GetComponent<Text>();
+	}
+	
+
+	void FixedUpdate () {
+		dateTime = GameMaster.instance.currentTime + ":00";
+		if (displayDate)
+			dateTime = "Day: " + GameMaster.instance.currentDay + "   " + dateTime;
+		clockDisplay.text = dateTime;
+	}
+}
