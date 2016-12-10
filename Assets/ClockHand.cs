086e398e77b8b6c58cbc7c1142075a0463a4e5a4
Liam Collins <liamcollins333@gmail.com>
2016-12-10 18:58:27 -0500

    functional clock

---
+++ Assets/ClockHand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockHand : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int time = GameMaster.instance.currentTime;
+		float rotation = 30 * (time % 12);
+		transform.localRotation = Quaternion.Euler (0, 90 - rotation, -90);
+		// this is pretty shitty, dkm
+	}
+}
