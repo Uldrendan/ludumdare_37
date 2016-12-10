ab37e23efc1a18c5a195722a431bb7a493a62c5e
Liam Collins <liamcollins333@gmail.com>
2016-12-09 22:56:05 -0500

    laying groundwork for game code

---
+++ Assets/TestCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCube : Interactable {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void OnClick() {
+		Debug.Log ("I'm a cube, ya bish");
+		CurrentCoroutine = TestCoroutine();
+	}
+
+	IEnumerator TestCoroutine () {
+		Vector3 randomPosition = new Vector3 (Random.Range (-5f, 5f), Random.Range (-5f, 5f), 0);
+		while (true) {
+			transform.position = Vector3.MoveTowards (transform.position, randomPosition, 1 * Time.deltaTime);
+			yield return null;
+		}
+	}
+}
