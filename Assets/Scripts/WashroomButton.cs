2d0194044e1dc5f6fdc3fc3017b176b7d5773064
Liam Collins <liamcollins333@gmail.com>
2016-12-11 00:38:09 -0500

    ...ok

---
+++ Assets/Scripts/WashroomButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WashroomButton : MonoBehaviour {
+
+	GameObject contextMenu;
+
+	void Start()
+	{
+		contextMenu = transform.parent.gameObject;
+	}
+
+	public void BathroomFunction()
+	{
+		switch (gameObject.name)
+		{
+			case "Toilet":
+				GameMaster.instance.Bathroom += 100;
+				break;
+			case "Brush":
+				GameMaster.instance.Hygiene += 20;
+				break;
+			case "Shower":
+				GameMaster.instance.Hygiene += 30;
+				break;
+		}
+		contextMenu.SetActive(false);
+		GameMaster.instance.ClearContext();
+	}
+}
