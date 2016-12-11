43c85bc0bf8a1f3c06e1e09f3b583b152b923f4d
Daniel Clarke <danclarke88@gmail.com>
2016-12-10 22:49:23 -0500

    Implemented display to show current money

---
+++ Assets/Scripts/MoneyDisplay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyDisplay : MonoBehaviour {
+
+	void Update () {
+		GetComponent<Text>().text = "$" + GameMaster.instance.money.ToString("0.00");
+	}
+}
