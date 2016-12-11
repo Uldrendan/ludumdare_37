5fe45583487143b03e5a7ab62607a8ffe6a85cd6
Liam Collins <liamcollins333@gmail.com>
2016-12-11 00:36:03 -0500

    A bunch of product functionality

---
+++ Assets/Product.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Product : ScriptableObject {
+	public string Name;
+	public string Description;
+	public float Cost;
+	public Sprite Icon;
+
+	public abstract void OnUse ();
+}
+
+public class SaladProduct : Product {
+	public SaladProduct () {
+		Name = "Salad";
+		Description = "The healthy alternative.";
+		Cost = 20;
+		Icon = Resources.Load ("salad_icon") as Sprite;
+	}
+
+	public override void OnUse () {
+
+	}
+}
+
+public class HotPocketsProduct : Product {
+	public HotPocketsProduct () {
+		Name = "Hot Pockets";
+		Description = "Scalding hot, then freezing cold.";
+		Cost = 5;
+		Icon = Resources.Load ("salad_icon") as Sprite;
+	}
+
+	public override void OnUse () {
+
+	}
+}
+
+public class ColdPizzaProduct : Product {
+	public ColdPizzaProduct () {
+		Name = "Cold Pizza";
+		Description = "An explosion of flavour!";
+		Cost = 10;
+		Icon = Resources.Load ("salad_icon") as Sprite;
+	}
+
+	public override void OnUse () {
+
+	}
+}
---
+++ Assets/ShopItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour {
+
+	public Product Product;
+	public Image Icon;
+	public Text Cost;
+	public Text Name;
+	public Text Description;
+
+	public void Start () {
+		Setup ();
+	}
+
+	public void SetProduct(Product product) {
+		Product = product;
+		Name.text = Product.Name;
+		Description.text = Product.Description;
+		Cost.text = "$" + Product.Cost;
+		Icon.sprite = Product.Icon;
+
+	}
+
+	public void Setup() {
+		Icon = transform.Find ("Icon").GetComponent<Image> ();
+		Cost = transform.Find ("Cost").GetComponent<Text> ();
+		Name = transform.Find ("Name").GetComponent<Text> ();
+		Description = transform.Find ("Description").GetComponent<Text> ();
+		transform.localScale = new Vector3 (1, 1, 1);
+	}
+}
