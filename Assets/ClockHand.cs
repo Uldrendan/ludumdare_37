c96742c354e2fdeac26d7dd396075d55d4706c45
Liam Collins <liamcollins333@gmail.com>
2016-12-10 19:09:12 -0500

    Added sound effect for clock

--- Assets/ClockHand.cs
+++ Assets/ClockHand.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class ClockHand : MonoBehaviour {
 
+	private AudioSource _audioSource;
+	private int _prevTime = 0;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		_audioSource = GetComponent<AudioSource> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		int time = GameMaster.instance.currentTime;
-		float rotation = 30 * (time % 12);
-		transform.localRotation = Quaternion.Euler (0, 90 - rotation, -90);
-		// this is pretty shitty, dkm
+		if (_prevTime != time) {
+			float rotation = 30 * (time % 12);
+			transform.localRotation = Quaternion.Euler (0, 90 - rotation, -90);
+			// this is pretty shitty, dkm
+			_audioSource.Play();
+		}
+
+		_prevTime = time;
 	}
 }
