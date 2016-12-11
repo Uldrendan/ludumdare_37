12142ec3dde70a9c2c615ac0b5100f65dbc0c4cd
Susie <hwang.sus@gmail.com>
2016-12-11 16:22:46 -0500

    all work and all play

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 // Handles character behaviour and positioning in game world
 public class Character : MonoBehaviour {
+    
+    public enum Locations { Desk, Bed, Washroom, Kitchen, FrontDoor, Center }
 
-    public enum ComputerState { Idle, Gaming, Working }
-    public enum Locations { Desk, Bed, Washroom, Kitchen, FrontDoor, Center}
+    private Animator _animator;
 
     // set in inspector with children of Waypoints prefab, same order as enum Locations
     public Transform[] Points; 
 
 	// Use this for initialization
 	void Start () {
+        _animator = this.GetComponent<Animator>();
+        Idle();
+    }
+
+    // Update is called once per frame
+    void Update () {
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void Idle()
+    {
+        _animator.SetInteger("computerMode", 0);
+    }
+
+    public void Play()
+    {
+        _animator.SetInteger("computerMode", 1);
+    }
+
+    public void Work() {
+        _animator.SetInteger("computerMode", 2);
+    }
 }
