9480d801e6d4e67f5551058ff53a23cad826da55
Liam Collins <liamcollins333@gmail.com>
2016-12-11 17:04:57 -0500

    inventory system integrated with amazon

---
+++ Assets/KitchenContextMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenContextMenu : MonoBehaviour {
+
+	public GameObject HotPocketButton_GO;
+	public GameObject ColdPizzaButton_GO;
+	public GameObject GardenSaladButton_GO;
+
+	private UseProductButton _hotPocketButton;
+	private UseProductButton _coldPizzaButton;
+	private UseProductButton _gardenSaladButton;
+
+	// Use this for initialization
+	void Start () {
+		_hotPocketButton = HotPocketButton_GO.GetComponent<UseProductButton> ();
+		_hotPocketButton.SetProduct(new HotPocketsProduct());
+		_coldPizzaButton = ColdPizzaButton_GO.GetComponent<UseProductButton> ();
+		_coldPizzaButton.SetProduct(new ColdPizzaProduct());
+		_gardenSaladButton = GardenSaladButton_GO.GetComponent<UseProductButton> ();
+		_gardenSaladButton.SetProduct (new SaladProduct ());
+	}
+}
---
+++ Assets/Scripts/UseProductButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseProductButton : Button {
+
+	GameObject contextMenu;
+	public Product Product;
+	private Text _text;
+
+	void Start()
+	{
+		contextMenu = transform.parent.gameObject;
+		_text = GetComponentInChildren<Text> ();
+		onClick.AddListener (() => UseProduct ());
+	}
+
+	void Update () {
+		if (GameMaster.instance.GetStock (Product) > 0) {
+			interactable = true;
+		} else {
+			interactable = false;
+		}
+		_text.text = Product.Name + " x" + GameMaster.instance.GetStock (Product);
+
+	}
+
+	public void UseProduct () {
+		if (GameMaster.instance.GetStock (Product) > 0) {
+			GameMaster.instance.UseProduct (Product);
+		}
+		contextMenu.SetActive(false);
+		GameMaster.instance.ClearContext();
+	}
+
+	public void SetProduct (Product product) {
+		Product = product;
+	}
+}
