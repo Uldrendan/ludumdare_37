ca992604cd82aa2a24f27ba50357b7ef645e62d6
Daniel Clarke <danclarke88@gmail.com>
2016-12-11 01:59:27 -0500

    Implementation of kitchen context menu

---
+++ Assets/Scripts/KitchenButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenButton : MonoBehaviour {
+
+	GameObject contextMenu;
+
+	void Start()
+	{
+		contextMenu = transform.parent.gameObject;
+	}
+
+	public void EatSalad()
+	{
+		SaladProduct salad = new SaladProduct();
+		salad.OnUse();
+		contextMenu.SetActive(false);
+		GameMaster.instance.ClearContext();
+	}
+
+	public void EatPocket()
+	{
+		HotPocketsProduct pocket = new HotPocketsProduct();
+		pocket.OnUse();
+		contextMenu.SetActive(false);
+		GameMaster.instance.ClearContext();
+	}
+
+	public void EatPizza()
+	{
+		ColdPizzaProduct pizza = new ColdPizzaProduct();
+		pizza.OnUse();
+		contextMenu.SetActive(false);
+		GameMaster.instance.ClearContext();
+	}
+}
